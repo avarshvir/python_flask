f15e26724930a866ed974247ab314471adaaaf5c
avarshvir <avarshvir@gmail.com>
2025-03-14 22:31:47 +0530

    template

---
+++ 2_template/1_template.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/printTable/<int:number>')
+def printTable(number):
+    return render_template('table.html',num=number)
+
+if __name__ == ('__main__'):
+    app.run(debug=True)

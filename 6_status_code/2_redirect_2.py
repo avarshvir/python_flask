aced74a003569a846367ac722b351a2dd3bc7060
avarshvir <avarshvir@gmail.com>
2025-04-08 23:20:34 +0530

    redirect in flask

---
+++ 6_status_code/2_redirect_2.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, redirect, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/redirectedPage')
+def redirected_page():
+    print("Request Arguments again from redirected page: ", request.args)
+    return "This is redirected page"
+
+@app.route('/redirectDemo')
+def redirect_demo():
+    print("Request Arguments from Demo: ", request.args)
+    res = redirected_page()
+    return res
+    #return redirect(url_for('redirected_page'))
+
+
+if __name__ == ('__main__'):
+    app.run(debug=True)
